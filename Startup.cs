07e3380b167cac10bf07453be33dcba97bf7fdb0
bryanlam549 <bryanlam549@hotmail.com>
2020-12-11 21:26:03 -0700

    Added the create button

--- Startup.cs
+++ Startup.cs
@@ -55,7 +55,7 @@ namespace CPSC571Project6
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Home}/{action=Topics}/{id?}");
                 endpoints.MapRazorPages();
             });
         }
