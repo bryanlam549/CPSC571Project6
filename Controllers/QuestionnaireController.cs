6f30a943ad90de266410cb44543b189674718d91
nicou08 <nicolas.guasca16@gmail.com>
2020-12-15 13:43:24 -0700

    Create Questionnaire Button

--- Controllers/QuestionnaireController.cs
+++ Controllers/QuestionnaireController.cs
@@ -24,13 +24,42 @@ namespace CPSC571Project6.Controllers
         public IActionResult Index(int? id)
         {
             var results = _db.Questionnaires.Where(x => x.topic_id == id).ToList();
+            ViewBag.topic_id = results.First().topic_id;
             return View(results);
         }
+
+        /*public IActionResult Questionnaire()
+        {
+            var results = _db.Questionnaires.ToList();
+            //results.Insert(0, new TopicClass { rowno = 0, subject_Name = "--Select Topic Name--" });
+            return View(results);
+        }*/
 
         public IActionResult Analyze(int? id)
         {
             var results = _db.Questionnaires.ToList();
             return View(results);
+        }
+
+        public IActionResult Create(int? tID)
+        {
+            ViewBag.tID = tID;
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(QuestionnaireClass newTitle)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Add(newTitle);
+                await _db.SaveChangesAsync();
+                //return RedirectToAction("Topics");
+                //return RedirectToAction("Index");
+                return RedirectToAction("Index", new { id = newTitle.topic_id });
+
+            }
+            return View(newTitle);
         }
 
 
